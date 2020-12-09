d2b6f71f181551f25ddea3d944d84814031809e5
Logan Madej <lnmadej23@gmail.com>
2020-12-09 12:54:17 -0500

    yes

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,20 @@
+// Enter your code below to edit index.html
+document.title = "About Me";
+
+yourName = "Logan Madej";
+
+document.getElementById("yourName").innerHTML = "Logan Madej";
+
+document.getElementById("myBio").innerHTML = "I Like Video Games"
+document.getElementById("collegeChoice").innerHTML = "I Would Like to go to Either Youngstown State or Kent State University"
+document.getElementById("programChoice").innerHTML = "I would like to study software engineering and Pilot School"
+document.getElementById("collegeReason").innerHTML = "I chose these because I love to code websites and web aplications also I always have wanted to learn to fly even if its not a career choice for me"
+document.getElementById("careerChoice").innerHTML = "Web Developer or Videogame Designer (If Web development doesn't work out)"
+document.getElementById("companyChoice").innerHTML = "Rukus Web Development"
+document.getElementById("careerReason").innerHTML = "Because they were the first company I saw"
+document.getElementById("q1").style.color = "blue";
+document.getElementById("answerOne").innerHTML = "I am dominating DOM"
+document.getElementById("q2").style.color = "blue";
+document.getElementById("answerTwo").innerHTML = "It can add variables and values to your page"
+document.getElementById("q3").style.color = "blue";
+document.getElementById("answerThree").innerHTML = "you can manipulate/change Id's and classes as well as many other things"
